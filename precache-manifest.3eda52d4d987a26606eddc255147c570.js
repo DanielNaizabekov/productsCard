38e2c2f6aa59f602c95eb80d7dad9a41919de5e6
Daniel <danielnaizabekov@gmail.com>
2020-08-25 18:32:34 +0600

    Updates

---
+++ precache-manifest.3eda52d4d987a26606eddc255147c570.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47492615120e86924da924f92d8280e2",
+    "url": "/productsCard/index.html"
+  },
+  {
+    "revision": "66a99b8fac2a6ea01bfb",
+    "url": "/productsCard/static/css/main.19246846.chunk.css"
+  },
+  {
+    "revision": "aac257a92ae3cd2a7ff9",
+    "url": "/productsCard/static/js/2.72b79e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/productsCard/static/js/2.72b79e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a99b8fac2a6ea01bfb",
+    "url": "/productsCard/static/js/main.eb33a334.chunk.js"
+  },
+  {
+    "revision": "eb06e8b64abaa9dda8f0",
+    "url": "/productsCard/static/js/runtime-main.84cae6b5.js"
+  },
+  {
+    "revision": "274da0c795b1d3e634a833cfaf2dccc7",
+    "url": "/productsCard/static/media/Exo2-Bold.274da0c7.woff2"
+  },
+  {
+    "revision": "2915e3a259a48342630bf1709e391b9d",
+    "url": "/productsCard/static/media/Exo2-Bold.2915e3a2.eot"
+  },
+  {
+    "revision": "3fc24e4b487c709ad7c1ba2b07a1da44",
+    "url": "/productsCard/static/media/Exo2-Bold.3fc24e4b.ttf"
+  },
+  {
+    "revision": "c25370c1608b1fb46d19fd7c6ea168f9",
+    "url": "/productsCard/static/media/Exo2-Bold.c25370c1.woff"
+  },
+  {
+    "revision": "add5818ba47217eeeed32ae1727a68e6",
+    "url": "/productsCard/static/media/Exo2-Regular.add5818b.woff"
+  },
+  {
+    "revision": "c5aba46dafb5591474476b479fb032b3",
+    "url": "/productsCard/static/media/Exo2-Regular.c5aba46d.ttf"
+  },
+  {
+    "revision": "c8e6798423c41648b23f896d6a5aadf1",
+    "url": "/productsCard/static/media/Exo2-Regular.c8e67984.eot"
+  },
+  {
+    "revision": "ee1dd19bf89a3472c238fe4959644322",
+    "url": "/productsCard/static/media/Exo2-Regular.ee1dd19b.woff2"
+  },
+  {
+    "revision": "105c364040b1414883d1fa5de3d43b16",
+    "url": "/productsCard/static/media/Exo2-Thin.105c3640.ttf"
+  },
+  {
+    "revision": "515e753570c5059fe1e13b8919de546d",
+    "url": "/productsCard/static/media/Exo2-Thin.515e7535.eot"
+  },
+  {
+    "revision": "aef8711a095a73edd4df3baf89e12d12",
+    "url": "/productsCard/static/media/Exo2-Thin.aef8711a.woff"
+  },
+  {
+    "revision": "e870567832a777e8c738dbae5b55b9e1",
+    "url": "/productsCard/static/media/Exo2-Thin.e8705678.woff2"
+  },
+  {
+    "revision": "28e5e56e21d06b3b2c5e86b4011b28c2",
+    "url": "/productsCard/static/media/bg.28e5e56e.png"
+  },
+  {
+    "revision": "17741b29c9c8fe02ad3cf47dc7ed6517",
+    "url": "/productsCard/static/media/cat.17741b29.png"
+  }
+]);
